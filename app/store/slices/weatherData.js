a7a1e734c4e2ef4930f9a053ca732604962edfcd
acl13 <128934431+acl13@users.noreply.github.com>
2025-01-07 11:27:50 -0500

    set up Redux store to fetch weather data from OpenWeatherAPI

---
+++ app/store/slices/weatherData.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchWeather = createAsyncThunk("fetchWeather", async () => {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=Charlotte&appid=${apiKey}`;
+  const response = await axios.get(url);
+  return response.data;
+});
+
+export const weatherSlice = createSlice({
+  name: "weather",
+  initialState: {
+    isLoading: false,
+    data: null,
+    error: false,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchWeather.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchWeather.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchWeather.rejected, (state) => {
+      state.error = true;
+    });
+  },
+});
+
+export default weatherSlice.reducer;
